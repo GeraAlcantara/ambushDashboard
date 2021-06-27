c09c6bbc3866bc0489ab80d2ccc0fa033b987ec3
Gerardo Alcantara <gerardo.mexicano@gmail.com>
2021-06-27 17:13:11 -0500

    sidabar resposnsive js to screenwidth

--- landingPage/static/landingPage/js/sidenav.js
+++ landingPage/static/landingPage/js/sidenav.js
@@ -1,11 +1,34 @@
 'use strict';
 
 let menu = document.querySelector('#menu');
-let sidebar = document.querySelector('.sidebar')
+let sidebar = document.querySelector('#Navbar')
 
 
 menu.addEventListener('click', function (event) {
     event.preventDefault();
     sidebar.classList.toggle('active')
-    console.log(e);
+    
 })
+
+window.addEventListener('resize', function () {
+    console.log('escuchando');
+    let windowScreen = this.screen.width;
+    let windowWidth = this.window.innerWidth
+    console.log(windowScreen, windowWidth);
+    if (windowScreen < 1200 || windowWidth < 1200) {
+        if (!sidebar.classList.contains('active')) {
+            return
+        }
+        console.log('has class');
+        sidebar.classList.remove('active')
+    } else if (windowScreen > 1200 || windowWidth > 1200) {
+        if (sidebar.classList.contains('active')) {
+            return
+        }
+        sidebar.classList.add('active')
+        
+    }
+    
+    
+})
+
