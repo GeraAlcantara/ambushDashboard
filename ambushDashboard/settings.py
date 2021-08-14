c54a99d9a640b9d18ae01aa5ce81eaf367b6d4a7
Gerardo Alcantara <gerardo.mexicano@gmail.com>
2021-08-13 19:29:12 -0500

    mimetype

--- ambushDashboard/settings.py
+++ ambushDashboard/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import mimetypes
 from pathlib import Path
 import environ
 import os
@@ -132,16 +133,23 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+
+mimetypes.add_type("text/css", ".css", True)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 
 STATIC_FILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+SASS_PROCESSOR_ROOT = STATIC_ROOT
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -152,9 +160,6 @@ STATICFILES_FINDERS = [
     'sass_processor.finders.CssFinder',
 ]
 
-STATIC_ROOT = BASE_DIR / 'static'
-
-SASS_PROCESSOR_ROOT = STATIC_ROOT
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
