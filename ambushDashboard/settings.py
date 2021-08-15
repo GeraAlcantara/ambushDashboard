2b0cb2129ee5e01fe5d3c0ca40ef8213bc8c14ae
Gerardo Alcantara <gerardo.mexicano@gmail.com>
2021-08-15 18:26:29 -0500

    change settings

--- ambushDashboard/settings.py
+++ ambushDashboard/settings.py
@@ -32,12 +32,11 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-
+SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be used, if set, instead)."
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=False)
+DEBUG = False
 
-ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS', default=[]))
+ALLOWED_HOSTS = []
 
 
 # Application definition
