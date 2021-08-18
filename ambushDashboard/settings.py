352e8316b40a264a9526c95f7430aa7ae76f3264
Gerardo Alcantara <gerardo.mexicano@gmail.com>
2021-08-18 16:32:01 -0500

    defaul true

--- ambushDashboard/settings.py
+++ ambushDashboard/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be used, if set, instead)."
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
