19cadf4523bdf80bb713977db12c5ceffa7fc0e1
Gerardo Alcantara <gerardo.mexicano@gmail.com>
2021-08-19 11:04:10 -0500

    change to lowercase

--- ambushDashboard/wsgi.py
+++ ambushDashboard/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ambushDashboard project.
+WSGI config for ambushdashboard project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ambushDashboard.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ambushdashboard.settings')
 
 application = get_wsgi_application()
