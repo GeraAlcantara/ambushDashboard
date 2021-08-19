8d7335c5ea78f4e4203a11dc3d3ff0c9aaa0c3dc
Gerardo Alcantara <gerardo.mexicano@gmail.com>
2021-08-19 10:24:05 -0500

    change databases

--- ambushDashboard/settings.py
+++ ambushDashboard/settings.py
@@ -89,11 +89,15 @@ WSGI_APPLICATION = 'ambushDashboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
-    }
+    'default': dj_database_url.config()
 }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
