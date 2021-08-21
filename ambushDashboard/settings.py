4750bf7ab7e84d04f4e93c14ae7e6d153ca3bfa2
Gerardo Alcantara <gerardo.mexicano@gmail.com>
2021-08-21 15:10:52 -0500

    update

--- ambushDashboard/settings.py
+++ ambushDashboard/settings.py
@@ -26,11 +26,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be used, if set, instead)."
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['.herokuapp.com', 'ambushDashboard.herokuapp.com']
+DEBUG = os.environ.get('DEBUG', False)
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', [])
 
 
 # Application definition
